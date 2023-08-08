68b6360224c494dcaa674efe171f0b6a79fadf6e
Alexey Baikov <baikov52@gmail.com>
2023-08-08 20:19:42 +0300

    test sentry url

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,11 @@ from django.urls import include, path
 from django.views.generic import TemplateView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
+
+def trigger_error(request):
+    division_by_zero = 1 / 0  # noqa
+
+
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     # Django Admin, use {% url 'admin:index' %}
@@ -23,6 +28,7 @@ urlpatterns += [
     ),
     path("api/auth/", include("djoser.urls")),
     path("api/auth/", include("djoser.urls.jwt")),
+    path("api/sentry-debug/", trigger_error),
 ]
 
 if settings.DEBUG:
